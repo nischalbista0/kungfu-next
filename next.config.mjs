ceddab6fb16dacb1400b8fb90917ab3b2ad92cac
Nischal Bista <nischalbista0@gmail.com>
2024-07-17 16:14:49 +0545

    testttt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: [
+    "kitchen-flow-editor",
+    "@ant-design/pro-editor",
+    "zustand",
+    "leva",
+  ],
+};
 
 export default nextConfig;
